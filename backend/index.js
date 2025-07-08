636f37202392cae1f0d84ebae331bfed95fe4b0f
Ankush <152962793+Ankush2213@users.noreply.github.com>
2025-07-08 17:15:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://doshopai-frontend-one.onrender.com" , "http://localhost:5174"],
+ origin:["https://doshopai-frontend-one.onrender.com" , "https://doshopai-admin.onrender.com"],
  credentials:true
 }))
 
